a9b43f14a081b701ae1f546d0746ca56ed8967c9
Alexey Dubnyak <alexey.dubnyak@graebert.com>
2025-12-11 13:08:49 +0100

    Встроен живой Ribbon компонент в EditorPage
    - Заменен скриншот риббона на реальный компонент из src/ribbon/Ribbon/
    - Подключен XML конфиг из src/config/ribbon-config.xml
    - Обновлены импорты токенов в index.css для поддержки всех компонентов риббона
    - Тема установлена на dark

--- src/components/Ribbon/Ribbon.jsx
+++ src/components/Ribbon/Ribbon.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
+import RibbonCore from '../../ribbon/Ribbon/Ribbon';
+import configPath from '../../config/ribbon-config.xml?url';
 import './Ribbon.css';
-import RibbonImage from '../../assets/editor/screenshots/ribbon.png';
 
+/**
+ * Обёртка для основного Ribbon, используемая на странице Editor.
+ * Рендерит живую ленту из XML-конфига.
+ */
 const Ribbon = () => {
-    return (
-        <div className="ribbon">
-            <div className="ribbon__scroll-container">
-                <img src={RibbonImage} alt="Ribbon" className="ribbon__image" />
-            </div>
-        </div>
-    );
+    return <RibbonCore configPath={configPath} theme="dark" />;
 };
 
 export default Ribbon;
